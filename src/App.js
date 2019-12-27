0428d07ee5032d6c740ea2443c1f282aa7bdb795
Abhigna Paruvella <aparuvella@deakin.edu.au>
2019-12-27 11:55:40 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CurrentConditions from './components/CurrentConditions';
 
-const API_KEY = "0100ceed70ca2e5c8d4143fee9e743e7" //26216 sample location key
+const API_KEY = "0100ceed70ca2e5c8d4143fee9e743e7"
 class App extends Component {
 
   render() {
